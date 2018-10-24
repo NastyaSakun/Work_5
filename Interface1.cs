26b5c58770d68105e35071bd153130ad9e1bbbb4
A <43045804+NastyaSakun@users.noreply.github.com>
2018-10-25 01:59:12 +0300

    Create Interface1.cs

---
+++ Interface1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_5
+{
+    interface Interface1
+    {
+        int Summ(int a, int b);
+
+        string Summ(string a);
+    }
+
+    interface Interface2
+    {
+        int Umn(int a, int b);
+    }
+}
